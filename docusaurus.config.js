8c6790579945cafb2361844800bea381983caf1f
Jefiozie <jefiozie.bosch@gmail.com>
2022-09-17 19:53:34 +0200

    chore: initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Validointi',
+  tagline: 'Template validation is cool',
+  url: 'https://validointi.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'validointi.github.io',
+  organizationName: 'validointi',
+  trailingSlash: false,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/jefiozie/validate-validation/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Validointi',
+        logo: {
+          alt: 'Validointi Logo',
+          src: 'img/angular.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'getting_started',
+            position: 'left',
+            label: 'Documentation',
+          },
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/jefiozie/validate-validation',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
